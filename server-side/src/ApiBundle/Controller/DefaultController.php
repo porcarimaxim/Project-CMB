2ca5b82f28d601946df58f25e1d9fd68cc8863e3
Osoian Marcel <osoian.marcel.d@gmail.com>
2015-04-26 22:48:34 +0300

    Example how to fetch users from database. Call it: app_dev.php/hello/Bean

--- server-side/src/ApiBundle/Controller/DefaultController.php
+++ server-side/src/ApiBundle/Controller/DefaultController.php
@@ -3,11 +3,20 @@
 namespace ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ApiBundle\Entity\Users;
+use ApiBundle\Entity\UsersRepository;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('ApiBundle:Default:index.html.twig', array('name' => $name));
-    }
+	public function indexAction($name)
+	{
+		$em = $this->getDoctrine()->getManager();
+		$usersRepository = $em->getRepository('ApiBundle:Users');
+		$usersRecords = $usersRepository->findAll();
+		return $this->render('ApiBundle:Default:index.html.twig',
+			array(
+				'name' => $name, 'users' => $usersRecords
+			)
+		);
+	}
 }
