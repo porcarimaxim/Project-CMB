3a32f9a615f24a0e0e5253c649ffa4f84eb21a0d
Osoian Marcel <osoian.marcel.d@gmail.com>
2015-04-26 23:28:39 +0300

    How to add new user record to table

--- server-side/src/ApiBundle/Controller/DefaultController.php
+++ server-side/src/ApiBundle/Controller/DefaultController.php
@@ -4,13 +4,20 @@ namespace ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use ApiBundle\Entity\Users;
-use ApiBundle\Entity\UsersRepository;
 
 class DefaultController extends Controller
 {
 	public function indexAction($name)
 	{
 		$em = $this->getDoctrine()->getManager();
+
+		$userModel = new Users();
+		$userModel->setEmail(time() . '@domain.md');
+		$userModel->setFirstName('New');
+		$userModel->setLastName('User');
+		$em->persist($userModel);
+		$em->flush();
+
 		$usersRepository = $em->getRepository('ApiBundle:Users');
 		$usersRecords = $usersRepository->findAll();
 		return $this->render('ApiBundle:Default:index.html.twig',
